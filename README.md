6284ad2b4148845ea498263850a7e38c8034952b
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:16 +0200

    update doc

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 [See available icons here](https://react-icons.deno.dev/fa)
 
-## install packahe
+## install the module
 
 ```bash
 deno add @preact-icons/fa
@@ -17,36 +17,15 @@ pnpm dlx jsr add @preact-icons/fa
 bunx jsr add @preact-icons/fa
 ```
 
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
-
-# Font Awesome icons for preact
-
-[![JSR](https://jsr.io/badges/@preact-icons/fa)](https://jsr.io/@preact-icons/fa)
-
-**License** [CC BY 4.0 License](https://creativecommons.org/licenses/by/4.0/)
-
-**Project** [https://fontawesome.com/](https://fontawesome.com/)
-
-[See available icons here](https://react-icons.deno.dev/fa)
-
-## install packahe
-
-```bash
-deno add @preact-icons/fa
-dnpx jsr add @preact-icons/fa
-pnpm dlx jsr add @preact-icons/fa
-bunx jsr add @preact-icons/fa
+You may need to update your preact mapping to avoid mixing JSR and http import:
+Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+```json
+{
+ "preact": "npm:preact@10.22.1",
+ "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ "preact/hooks": "npm:preact@10.22.1/hooks",
+}
 ```
-
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
 
 ## import an icon from all icons
 
@@ -65,4 +44,3 @@ or using default export
 ```ts
 import Fa500Px from "react-icons/fa/Fa500Px.ts"
 ```
-
--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/fa)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/fa
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/fa
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # Font Awesome icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/fa)](https://jsr.io/@preact-icons/fa)
- * 
- * **License** [CC BY 4.0 License](https://creativecommons.org/licenses/by/4.0/)
- * 
- * **Project** [https://fontawesome.com/](https://fontawesome.com/)
- * 
- * [See available icons here](https://react-icons.deno.dev/fa)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/fa
- * dnpx jsr add @preact-icons/fa
- * pnpm dlx jsr add @preact-icons/fa
- * bunx jsr add @preact-icons/fa
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import Fa500Px from "react-icons/fa/Fa500Px.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
